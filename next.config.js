673257c02ae737ef3db51fdd2097e264bf8526ef
waigo001 <kinwaka01@gmail.com>
2021-11-03 19:37:55 +0900

    add 環境変数

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     VERSION: require("./package.json").version,
+    ID_GITHUB: process.env.ID_GITHUB,
+    ID_TWITTER: process.env.ID_TWITTER,
+    ID_INSTAGRAM: process.env.ID_INSTAGRAM,
   },
   webpack: (config) => {
     config.module.rules.push({
