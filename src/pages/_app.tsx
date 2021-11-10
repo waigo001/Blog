a5143f76284165ea055b1c6aa3ea7a1f2b518a21
waigo001 <kinwaka01@gmail.com>
2021-11-10 12:48:27 +0900

    add emotion cache component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "src/styles/theme";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "src/lib/createEmotionCache";
+
+const cache = createEmotionCache();
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );  
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 };
 
 export default App;
