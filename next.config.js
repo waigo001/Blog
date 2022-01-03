e8bad0a0cfa289e1a03e4de24ef094eba568b93a
waigo001 <kinwaka01@gmail.com>
2022-01-03 12:48:27 +0900

    change next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  target: "serverless",
   pageExtensions: ["tsx", "ts", "jsx", "js", "mdx"],
   poweredByHeader: false,
   reactStrictMode: true,
