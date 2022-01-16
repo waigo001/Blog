806c1fed437fce19403aab4274c01a50e2b21cfc
waigo001 <kinwaka01@gmail.com>
2022-01-16 16:23:08 +0900

    add chakra-uiのprovider等の基本コンポーネント

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import theme from "src/styles/theme";
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}  portalZIndex={40}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "src/styles/theme";
 
 class MyDocument extends Document {
   render() {
@@ -8,11 +10,12 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.png" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&family=Noto+Sans+JP:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
         <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
