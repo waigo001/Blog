53b148bca9c06c9409728494dc73f30404d981a9
waigo001 <kinwaka01@gmail.com>
2022-01-24 17:55:37 +0900

    Google Tag Managerの追加 Fixes #14

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,13 @@ import type { AppProps } from "next/app";
 import theme from "src/styles/theme";
 import { DefaultSeo } from "next-seo";
 import nextSeoConfig from "next-seo.config";
+import GoogleTagManager from "src/components/GoogleTagManager";
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <DefaultSeo {...nextSeoConfig} />
-
+      <GoogleTagManager googleTagManagerId={process.env.ID_GTAG} />
       <ChakraProvider theme={theme} portalZIndex={40}>
         <Component {...pageProps} />
       </ChakraProvider>
