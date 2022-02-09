f3c0b0195214e4598547f32bc4869a81fef65097
waigo001 <kinwaka01@gmail.com>
2022-02-09 15:01:33 +0900

    add Toc view(PC)

--- src/components/uiParts/TableOfContent.tsx
+++ src/components/uiParts/TableOfContent.tsx
@@ -2,9 +2,10 @@ import {
   chakra,
   ListItem,
   OrderedList,
-  useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
+
+import { useScrollSpy } from "src/hooks/useScrollSpy";
 
 type Props = {
   toc: Toc[];
@@ -15,26 +16,34 @@ const TableOfContent: React.VFC<Props> = (props: Props) => {
   const linkColor = useColorModeValue("gray.500", "gray.400");
   const linkHoverColor = useColorModeValue("gray.900", "gray.100");
 
+  const extractToc = toc.filter((item) => item.depth === 2 || item.depth === 3);
+
+  const activeId = useScrollSpy(
+    extractToc.map(({ id }) => `[id="${id}"]`),
+
+    {
+      rootMargin: "0% 0% -80% 0%",
+    }
+  );
+
   return (
-    <OrderedList spacing={1} ml="0" mt="4" styleType="none">
-      {toc.map(({ text, id, depth }) => {
-        if (!(depth === 2 || depth === 3)) return;
-        return (
-          <ListItem key={id} title={text} ml={(depth - 1) * 4} my="2">
-            <chakra.a
-              href={`#${id}`}
-              fontSize="sm"
-              fontWeight="medium"
-              color={linkColor}
-              _hover={{
-                color: linkHoverColor,
-              }}
-            >
-              {text}
-            </chakra.a>
-          </ListItem>
-        );
-      })}
+    <OrderedList spacing={1} styleType="none">
+      {extractToc.map(({ text, id, depth }) => (
+        <ListItem key={id} title={text} ml={(depth - 1) * 4} my="2">
+          <chakra.a
+            href={`#${id}`}
+            fontSize="sm"
+            fontWeight={id === activeId ? "bold" : "medium"}
+            aria-current={id === activeId ? "location" : undefined}
+            color={id === activeId ? linkHoverColor : linkColor}
+            _hover={{
+              color: linkHoverColor,
+            }}
+          >
+            {text}
+          </chakra.a>
+        </ListItem>
+      ))}
     </OrderedList>
   );
 };
