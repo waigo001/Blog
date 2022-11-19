6edd142e95999179bb51e1807c4114e8d38c1778
waigo001 <kinwaka01@gmail.com>
2022-11-19 12:59:30 +0900

    update font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.png" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&family=Zen+Kaku+Gothic+New:wght@300;400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
